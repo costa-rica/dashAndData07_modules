fe0095f86d30bd12d8766d4151d36fe7aa4efe83
costa-rica <nrodrig1@gmail.com>
2023-06-14 19:22:49 +0200

    problems with db connection due to dd07_models config refrences to FLASK_ENV - corrected

--- dd07_modules/dd07_models/config.py
+++ dd07_modules/dd07_models/config.py
@@ -1,12 +1,12 @@
 import os
 from dd07_config import ConfigDev, ConfigProd, ConfigLocal
 
-if os.environ.get('CONFIG_TYPE')=='local':
+if os.environ.get('FLASK_ENV')=='local':
     config = ConfigLocal()
     print('- dd07_models/config: Local')
-elif os.environ.get('CONFIG_TYPE')=='dev':
+elif os.environ.get('FLASK_ENV')=='dev':
     config = ConfigDev()
     print('- dd07_models/config: Development')
-elif os.environ.get('CONFIG_TYPE')=='prod':
+elif os.environ.get('FLASK_ENV')=='prod':
     config = ConfigProd()
     print('- dd07_models/config: Production')
