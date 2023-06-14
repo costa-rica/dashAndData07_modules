9ddbf90daab741480f8a1acfb9c855f35f5335d9
costa-rica <nrodrig1@gmail.com>
2023-06-14 12:29:33 +0200

    .env includes db paths to local, dev, and prod; models/__init__.py no longer creates db or aux files --> now done by app_package/__init__.py

--- dd07_modules/dd07_config/config.py
+++ dd07_modules/dd07_config/config.py
@@ -14,15 +14,17 @@ class ConfigBasic():
 
     def __init__(self):
         self.SECRET_KEY = env_dict.get('SECRET_KEY')
-        self.DB_ROOT = os.environ.get('DB_ROOT')
+        self.DB_LOCAL_ROOT = os.environ.get('DB_LOCAL_ROOT')
+        self.DB_DEV_ROOT = os.environ.get('DB_DEV_ROOT')
+        self.DB_PROD_ROOT = os.environ.get('DB_PROD_ROOT')
         
 
         #Email stuff
         self.MAIL_SERVER = env_dict.get('MAIL_SERVER_MSOFFICE')
         self.MAIL_PORT = env_dict.get('MAIL_PORT')
         self.MAIL_USE_TLS = True
-        self.MAIL_USERNAME = env_dict.get('EMAIL')
-        self.MAIL_PASSWORD = env_dict.get('EMAIL_PASSWORD')
+        self.MAIL_USERNAME = env_dict.get('MAIL_EMAIL')
+        self.MAIL_PASSWORD = env_dict.get('MAIL_PASSWORD')
 
 
         #web Guest
@@ -42,32 +44,28 @@ class ConfigBasic():
         self.SECRET_KEY_CAPTCHA = env_dict.get('SECRET_KEY_CAPTCHA')
         self.VERIFY_URL_CAPTCHA = 'https://www.google.com/recaptcha/api/siteverify'
 
-        # Database
-        # self.SQL_URI = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_USERS')}"
-        # self.SQLALCHEMY_DATABASE_URI = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_USERS')}"
-        self.SQL_URI_USERS = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_USERS')}"
-        self.SQL_URI_CAGE = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_CAGE')}"
-        self.SQL_URI_BLS = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_BLS')}"
-        # self.SQLALCHEMY_BINDS = {}
-        # self.SQLALCHEMY_BINDS ={'Cage_bind':self.DB_CAGE,
-        #     'dbBls':self.DB_BLS}
+        # # Database
+        # self.SQL_URI_USERS = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_USERS')}"
+        # self.SQL_URI_CAGE = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_CAGE')}"
+        # self.SQL_URI_BLS = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_BLS')}"
         
-        # # other directories
-        self.DIR_DB_AUXILARY = os.path.join(self.DB_ROOT,"auxilary")
-        self.DIR_DB_AUX_IMAGES_PEOPLE = os.path.join(self.DIR_DB_AUXILARY,"images_people")
-        # self.WORD_DOC_DIR = config.get('WORD_DOC_DIR_MAC')
-        # self.PEOPLE_DIR = config.get('PEOPLE_DIR_MAC')
-        # setupSqlBinds()
-    
-    # def setupSqlBinds():
-    #     print("- accessed setupSqlBinds")
-    #     self.SQLALCHEMY_BINDS ={'dbCage':self.DB_CAGE,
-    #         'dbBls':self.DB_BLS}
+        # # # other directories
+        # self.DIR_DB_AUXILARY = os.path.join(self.DB_ROOT,"auxilary")
+        # self.DIR_DB_AUX_IMAGES_PEOPLE = os.path.join(self.DIR_DB_AUXILARY,"images_people")
+
 
 class ConfigLocal(ConfigBasic):
     
     def __init__(self):
         super().__init__()
+        # Database
+        self.DB_ROOT = self.DB_LOCAL_ROOT
+        self.SQL_URI_USERS = f"sqlite:///{self.DB_LOCAL_ROOT}{os.environ.get('DB_NAME_USERS')}"
+        self.SQL_URI_CAGE = f"sqlite:///{self.DB_LOCAL_ROOT}{os.environ.get('DB_NAME_CAGE')}"
+        self.SQL_URI_BLS = f"sqlite:///{self.DB_LOCAL_ROOT}{os.environ.get('DB_NAME_BLS')}"
+        # # other directories
+        self.DIR_DB_AUXILARY = os.path.join(self.DB_LOCAL_ROOT,"auxilary")
+        self.DIR_DB_AUX_IMAGES_PEOPLE = os.path.join(self.DIR_DB_AUXILARY,"images_people")
 
     DEBUG = True
     # TEMPLATES_AUTO_RELOAD = False
@@ -79,6 +77,14 @@ class ConfigDev(ConfigBasic):
 
     def __init__(self):
         super().__init__()
+        # Database
+        self.DB_ROOT = self.DB_DEV_ROOT
+        self.SQL_URI_USERS = f"sqlite:///{self.DB_DEV_ROOT}{os.environ.get('DB_NAME_USERS')}"
+        self.SQL_URI_CAGE = f"sqlite:///{self.DB_DEV_ROOT}{os.environ.get('DB_NAME_CAGE')}"
+        self.SQL_URI_BLS = f"sqlite:///{self.DB_DEV_ROOT}{os.environ.get('DB_NAME_BLS')}"
+        # # other directories
+        self.DIR_DB_AUXILARY = os.path.join(self.DB_DEV_ROOT,"auxilary")
+        self.DIR_DB_AUX_IMAGES_PEOPLE = os.path.join(self.DIR_DB_AUXILARY,"images_people")
 
     DEBUG = True
     # SQL_URI = env_dict.get('SQL_URI_DEVELOPMENT')
@@ -90,6 +96,14 @@ class ConfigProd(ConfigBasic):
         
     def __init__(self):
         super().__init__()
+        # Database
+        self.DB_ROOT = self.DB_PROD_ROOT
+        self.SQL_URI_USERS = f"sqlite:///{self.DB_PROD_ROOT}{os.environ.get('DB_NAME_USERS')}"
+        self.SQL_URI_CAGE = f"sqlite:///{self.DB_PROD_ROOT}{os.environ.get('DB_NAME_CAGE')}"
+        self.SQL_URI_BLS = f"sqlite:///{self.DB_PROD_ROOT}{os.environ.get('DB_NAME_BLS')}"
+        # # other directories
+        self.DIR_DB_AUXILARY = os.path.join(self.DB_PROD_ROOT,"auxilary")
+        self.DIR_DB_AUX_IMAGES_PEOPLE = os.path.join(self.DIR_DB_AUXILARY,"images_people")
 
     DEBUG = False
     TESTING = False
