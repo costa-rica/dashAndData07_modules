f43659d4f5981c8d6340c637be10e9629e37a470
costa-rica <nrodrig1@gmail.com>
2023-06-16 13:23:30 +0200

    fixed dd07_models/main.py sess changed to sees_users

--- dd07_modules/dd07_models/main.py
+++ dd07_modules/dd07_models/main.py
@@ -21,5 +21,5 @@ login_manager.login_message_category = 'info'
 @login_manager.user_loader
 def load_user(any_name_for_id_obj):# any_name_for_id_obj can be any name because its an arg that is the user id.
     # This is probably created somewhere inside flask_login when the user gets logged in. But i've not been able to track it.
-    return sess.query(Users).filter_by(id = any_name_for_id_obj).first()
+    return sess_users.query(Users).filter_by(id = any_name_for_id_obj).first()
 
